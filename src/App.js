67e13327f60bc0a20b3f0fa0b8a9f4b5bd5fa962
Bill D <355438+vegetabill@users.noreply.github.com>
2020-03-23 14:33:42 -0700

    just mutated state because oh well

--- src/App.js
+++ src/App.js
@@ -2,35 +2,38 @@ import React, { Component } from "react";
 import TodoList from "./components/TodoList";
 import "./App.css";
 
-const STARTER_STATE = {
-  todos: [
-    {
-      text: "Download create-react-app",
-      created: new Date(),
-      isCompleted: true,
-      id: 1
-    },
-    {
-      text: "Learn about one-way data flow in React",
-      created: new Date(),
-      isCompleted: false,
-      id: 2
-    }
-  ]
-};
-
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = STARTER_STATE;
+    this.state = {
+      todos: [
+        {
+          text: "Download create-react-app",
+          created: new Date(),
+          isCompleted: true,
+          id: 1
+        },
+        {
+          text: "Learn about one-way data flow in React",
+          created: new Date(),
+          isCompleted: false,
+          id: 2
+        },
+        {
+          text: "Learn Zoom breakout rooms",
+          created: new Date(),
+          isCompleted: false,
+          id: 3
+        }
+      ]
+    };
   }
   render() {
     const actions = {
       updateTodoCompleted: (todoId, isCompleted) => {
-        const todosCopy = [].concat(this.state.todos);
-        const toModify = todosCopy.find(todo => todo.id === todoId);
-        toModify.isCompleted = isCompleted;
-        this.setState({ todos: todosCopy });
+        const idx = this.state.todos.findIndex(todo => todoId === todo.id);
+        this.state.todos[idx].isCompleted = isCompleted;
+        this.setState({});
       }
     };
     return <TodoList todos={this.state.todos} actions={actions} />;
