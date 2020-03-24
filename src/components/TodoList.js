5f53e8bc93e984b32dcb45c44d76ad1be99798ab
Robyn Beck <57880782+o-Outlandish1@users.noreply.github.com>
2020-03-23 17:11:38 -0700

    Update to toggle method and added sort by date
    Ok, the last commit was the wrong file.. Here is the proper file that actually contains the update to the toggle method and adding sort by date

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,59 @@
+
 import React, { Component } from "react";
-class Todo extends Component {
+import Todo from "./Todo";
+class TodoList extends Component {
+  constructor(props)
+  {
+    super();
+    this.state = {hideCompleted : false}
+    this.onHideToggle = this.onHideToggle.bind(this);  
+    // this.isSorted = this.isSorted.bind(this);  
+
+  }
+
+onHideToggle()
+{
+ let newHideCompleted = !this.state.hideCompleted;
+  this.setState({hideCompleted: newHideCompleted});
+}
+
+sortByDate()
+{
+  this.setState({isSorted: true})
+}
+  
   render() {
-    const classes = ["todo"];
-    const { id, isCompleted, text } = this.props.todo;
-    if (isCompleted) {
-      classes.push("completed");
-    }
-    const onToggle = () => {
-      const newValue = !isCompleted;
-      this.props.updateTodoCompleted(id, newValue);
-    };
+    let { todos, actions } = this.props;
+  if(this.state.hideCompleted) {todos = todos.filter(todo => !todo.isCompleted)
+  }
+    // if(this.state.isSorted)
+    // {todos = todos.sort(todo => )} 
+  
+
     return (
-      <div className={classes.join(" ")}>
-        <label>
-          <input type="checkbox" checked={isCompleted} onChange={onToggle} />
-          {text}
+      <div>
+        <h3>TODOs ({todos.length})</h3>
+          <label>
+          <input type="checkbox" 
+            checked={this.state.hideCompleted} 
+            onChange={this.onHideToggle} />
+          Hide Completed ToDos
         </label>
+        <button 
+        onClick={this.sortByDate}>
+          Sort By Date
+        </button>
+      
+        {todos.map(todo => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            updateTodoCompleted={actions.updateTodoCompleted}
+          />
+        ))}
       </div>
     );
   }
 }
 
-export default Todo;
+export default TodoList;
